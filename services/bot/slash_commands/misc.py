f0063338ad554465545112646d37967bee927963
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2026-01-25 20:55:57 -0800

    Added bot logging for irrelevant/gemini fail interactions

--- services/bot/slash_commands/misc.py
+++ services/bot/slash_commands/misc.py
@@ -507,11 +507,16 @@ class ProcessModal(discord.ui.Modal, title="Submit Process Timeline"):
 
         if processed_timeline == "Not relevant":
             await interaction.followup.send("Your description was not relevant!", ephemeral=True)
+            sys_msg = f"{self.username} has tried to add a process timeline for a company but the description was not relevant."
+            await self.bot_context.log(self.bot, sys_msg)
             return
         
         if processed_timeline == "Request failed. Please try again later.":
             await interaction.followup.send("Request failed. Please try again later.", ephemeral=True)
-
+            sys_msg = f"{self.username} has tried to add a process timeline for a company but the Gemini request failed."
+            await self.bot_context.log(self.bot, sys_msg)
+            return
+        
         embed = discord.Embed(title=f"Process for {company_name}", color=discord.Color.blue())
         embed.add_field(name="Company:", value=company_name, inline=True)
         embed.add_field(name="Role:", value=role, inline=True)
